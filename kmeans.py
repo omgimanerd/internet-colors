7abb1c56d263dcd67048f34c66f36713e55579d2
omgimanerd <alvin.lin.dev@gmail.com>
2017-05-30 00:16:17 -0700

    converted kmeans algorithm to numpy

--- kmeans.py
+++ kmeans.py
@@ -2,59 +2,47 @@
 # Kmeans clustering algorithm heavily influenced by
 # https://gist.github.com/iandanforth/5862470
 
-from util import get_color_distance
+import numpy as np
 
-import random
-
-def get_centroid(colors, weights):
+def _get_centroid(colors):
     """
-    Given a list of colors and a parallel list of weights, this function
-    returns the centroid of the points.
+    Given a list of colors, this function returns the centroid of the colors.
     """
-    for i, weight in enumerate(weights):
-        colors[i] = [c * weight for c in colors[i]]
-    rgb = zip(*colors)
-    wsum = sum(weights)
-    return [sum(v) / wsum for v in rgb]
+    return colors.mean(axis=0)
 
-def get_closest_centroid_index(p, centroids):
+def _get_index_closest(p, centroids):
     """
     Given a point p and list of centroids, this function returns the index
     of the centroid in the list that p is closest to.
     """
-    index = 0
-    min_distance = get_color_distance(p, centroids[0])
-    for i in range(1, len(centroids)):
-        distance = get_color_distance(p, centroids[i])
-        if distance < min_distance:
-            min_distance = distance
-            index = i
-    return index
+    return np.argmin(((centroids - p) ** 2).sum(axis=1))
 
 def kmeans(k, colors, weights, cutoff):
     """
-    Given a k value, a list of colors, a parallel containing the weights
+    Given a k value, a list of colors, a parallel list containing the weights
     of those colors, and a centroid shift cutoff value, this function will
     return k centroids
     """
-    centroids = random.sample(colors, k)
+    # Multiply the colors by their weights
+    colors = np.array(colors) * np.array(weights).reshape(len(weights), 1)
+    # Pick k random colors as starting centroids
+    centroids = colors[np.random.randint(colors.shape[0], size=k),:]
+    clusters = None
     biggest_shift = cutoff + 1
-    j = 0
     while biggest_shift > cutoff:
-        clusters = [[] for i in centroids]
-        cluster_weights = [[] for i in centroids]
-        shifts = [[] for i in centroids]
-        for color, weight in zip(colors, weights):
-            index = get_closest_centroid_index(color, centroids)
-            clusters[index].append(color)
-            cluster_weights[index].append(weight)
-        for i, cluster in enumerate(clusters):
-            new_centroid = centroids[i]
-            if len(cluster) > 0:
-                new_centroid = get_centroid(cluster, cluster_weights[i])
-            shifts[i] = get_color_distance(new_centroid, centroids[i])
-            centroids[i] = new_centroid
-        biggest_shift = max(shifts)
+        # Calculate which centroid each color is closest to. This generates an
+        # array of indices representing which centroid the point is closest to.
+        # This array is parallel to the points array.
+        closest = np.array([_get_index_closest(c, centroids) for c in colors])
+        # Get the new centroids by calculating the centroid of the point cluster
+        # for each centroid.
+        clusters = np.array(
+            [colors[closest == k] for k in range(centroids.shape[0])])
+        new_centroids = np.array([cluster.mean(axis=0) for cluster in clusters])
+        # Calculate the amount that the new centroids shifted. When this amount
+        # is lower than a specified threshold, then we stop the algorithm.
+        biggest_shift = ((new_centroids - centroids) ** 2).sum(axis=0).min()
+        centroids = new_centroids
     return centroids, clusters
 
 if __name__ == '__main__':
@@ -68,9 +56,9 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     centers = [[25, 25, 25], [100, 100, 100], [2, 57, 20]]
-    p, l = make_blobs(n_samples=100, centers=centers, cluster_std=5, random_state=0)
-    w = [1 for i in range(100)]
-    centroids, clusters = kmeans(3, list(p), w, 2)
+    p, l = make_blobs(n_samples=100, centers=centers, cluster_std=5,
+                      random_state=0)
+    centroids, clusters = kmeans(3, list(p), np.ones(100), 2)
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     r, g, b = zip(*p)
--- plot_3d.py
+++ plot_3d.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 
 from util import norm_rgb
+from kmeans import *
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,14 +28,41 @@ def get_data():
                 frequencies.append(data[key])
     return colors, frequencies
 
-def plot(colors, frequencies):
+def plot_color_frequencies(colors, frequencies):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     r, g, b = zip(*colors)
     m = sorted(frequencies)[-10]
     sizes = list(map(lambda x: clamp(x / m, 0.05, 1) * MAX_POINT_SIZE, frequencies))
-    colors = list(map(norm_rgb, colors))
-    ax.scatter(r, g, b, c=colors, s=sizes)
+    ax.scatter(r, g, b, c=[norm_rgb(color) for color in colors], s=sizes)
+    ax.set_xlabel('R')
+    ax.xaxis.label.set_color('red')
+    ax.set_ylabel('G')
+    ax.yaxis.label.set_color('green')
+    ax.set_zlabel('B')
+    ax.zaxis.label.set_color('blue')
+    ax.set_xlim(0, 256)
+    ax.set_xticks(range(0, 257, 32))
+    ax.tick_params(axis='x', colors='red')
+    ax.set_ylim(0, 256)
+    ax.set_yticks(range(0, 257, 32))
+    ax.tick_params(axis='y', colors='green')
+    ax.set_zlim(0, 256)
+    ax.set_zticks(range(0, 257, 32))
+    ax.tick_params(axis='z', colors='blue')
+    return fig, ax
+
+def plot_color_clusters(colors, frequencies):
+    centroids, clusters = kmeans(10, colors, frequencies, 0.5)
+    print(centroids, clusters)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    r, g, b = zip(*centroids)
+    colors = [norm_rgb(color) for color in centroids]
+    ax.scatter(r, g, b, c=colors)
+    for cluster in clusters:
+        r, g, b = zip(*cluster)
+        ax.scatter(r, g, b, c=colors)
     ax.set_xlabel('R')
     ax.xaxis.label.set_color('red')
     ax.set_ylabel('G')
@@ -54,9 +82,16 @@ def plot(colors, frequencies):
 
 if __name__ == '__main__':
     colors, frequencies = get_data()
-    fig, ax = plot(colors, frequencies)
-    ax.view_init(30, 0)
-    def update(i):
-        ax.view_init(30, i)
-    anim = FuncAnimation(fig, update, frames=np.arange(0, 360), interval=5)
-    anim.save('output/plot_3d.gif', dpi=80, writer='imagemagick')
+    fig, ax = plot_color_clusters(colors, frequencies)
+    plt.show()
+    # ax.view_init(30, 0)
+    # def update(i):
+    #     ax.view_init(30, i)
+    # anim = FuncAnimation(fig, update, frames=np.arange(0, 360), interval=5)
+    # anim.save('plot_3d_clusters.gif', dpi=80, writer='imagemagick')
+    # fig, ax = plot_color_frequencies(colors, frequencies)
+    # ax.view_init(30, 0)
+    # def update(i):
+    #     ax.view_init(30, i)
+    # anim = FuncAnimation(fig, update, frames=np.arange(0, 360), interval=5)
+    # anim.save('output/plot_3d.gif', dpi=80, writer='imagemagick')
